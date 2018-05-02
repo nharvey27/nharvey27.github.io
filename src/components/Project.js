153989432184def098f5010eb1ebb3e9ea9b74ac
Nicholas Harvey <nharvey278@gmail.com>
2018-05-02 12:08:00 -0700

    update

--- src/components/Project.js
+++ src/components/Project.js
@@ -48,7 +48,7 @@ class Project extends React.Component {
               </div>
               <div className="project__description-container col-md-10 col-md-offset-1">
                 <p className="project__description">
-                  View top 50 cryptocurrencies price, market cap and 24 hour
+                  View the top 50 cryptocurrencies price, market cap and 24 hour
                   change percentage. Users may also view price graphs for 7, 30,
                   180 and 365 days.
                 </p>
@@ -72,9 +72,9 @@ class Project extends React.Component {
               </div>
               <div className="project__description-container col-md-10 col-md-offset-1">
                 <p className="project__description">
-                  Dex is a Pokexdex of the original 151 Pokemon. It is a RESTFUl
-                  API written with Ruby On Rails with Postgresql as the
-                  database. The Front-End is written with React.
+                  Pokexdex of the original 151 Pokemon. It is a REST API written
+                  using Ruby On Rails with Postgresql as the database. The
+                  Front-End is written with React.
                 </p>
               </div>
               <div className="project__links-container col-md-6 col-md-offset-3">
@@ -97,9 +97,8 @@ class Project extends React.Component {
               </Link>
               <div className="project__description-container col-md-10 col-md-offset-1">
                 <p className="project__description">
-                  Material Bootstrap is a redesign of the pre-existing bootstrap
-                  components with a Material-UI inspired flair. It was written
-                  with SCSS.
+                  Material Bootstrap is a redesign of the bootstrap components
+                  with a Material-UI inspired flair. It was written with SCSS.
                 </p>
               </div>
               <div className="project__links-container col-md-6 col-md-offset-3">
@@ -121,8 +120,8 @@ class Project extends React.Component {
               </div>
               <div className="project__description-container col-md-10 col-md-offset-1">
                 <p className="project__description">
-                  Daily News is a news aggregator of many major news stations.
-                  It is built with React, Redux and multiple other Javascript
+                  Daily News is a news aggregator of major news stations. It is
+                  built with React, Redux and multiple other Javascript
                   Ecosystem tools. It is styled beautifly with the Semantic-UI
                   library.
                 </p>
